2e8aa3a1bf121f3205d1747435da9ff8edada01a
Jack <jhkent88@gmail.com>
2018-08-01 22:49:19 -0700

    Make functional for madonna only, no win/lose yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,37 +5,28 @@ var guessesArray = [];
 var secretWord = ["m","a","d","o","n","n","a"];
 var workingWord = ["_","_","_","_","_","_","_"];
 
+document.querySelector("#workingWordText").innerHTML = workingWord;
 
 document.onkeydown = function(event) {
 
     var keyPressed = event.key.toLowerCase();
-    // guessesArray.push(" " + keyPressed);
     var guessIndex = secretWord.indexOf(keyPressed);
 
     if (guessIndex < 0) {
         guessesArray.push(keyPressed);
-    }
-
-    for (i = 0; )
-
-
-
-    if (keyPressed === compChoice) {
-        wins++;
-        guessesLeft = 8;
-        guessesArray = [];
-    }
-    
-    else {
         guessesLeft--;
-        if (guessesLeft === 0) {
-            losses++;
-            guessesLeft = 8;
-            guessesArray = [];
-        };
+    }
+
+    else {        
+        for (i = 0; i < secretWord.length; i++) {
+            if (secretWord[i] === keyPressed) {
+                workingWord[i] = keyPressed;
+            };
+        }
     };
 
-    
+
+    document.querySelector("#workingWordText").innerHTML = workingWord;
     document.querySelector("#winsText").innerHTML = "Wins: " + wins;
     document.querySelector("#lossesText").innerHTML = "Losses: " + losses;
     document.querySelector("#guessesLeftText").innerHTML = "Guesses left: " + guessesLeft;
