a2e258b0ace4682ce2f4f0bf9a97005ff7425cd0
Jack <jhkent88@gmail.com>
2018-08-02 23:35:44 -0700

    Finish for HW submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,97 @@
 var wins = 0;
 var losses = 0;
-var guessesLeft = 8;
-var guessesArray = [];
-var secretWord = ["m","a","d","o","n","n","a"];
-var workingWord = ["_","_","_","_","_","_","_"];
+var guessesLeft = 10;
+var badLettersArray = [];
+var wordList = ["monkey","scumm","banana","pirate","ghost","sword","grog","island","curse","guybrush","acetone","ship","sail","skeleton","skull","accounting","mansion","meat","fish","seagull","cannibal","voodoo","prisoner","voyage","sheriff","prosthetic","rubber","chicken"]
+var music = new Audio("assets/audio/05 - Following The Shop Keeper.mp3");
+var yesSound = new Audio("assets/audio/sfx_coin_double1.m4a");
+var noSound = new Audio("assets/audio/sfx_exp_various3.m4a");
+var winSound = new Audio("assets/audio/sfx_sounds_fanfare3.m4a");
+var loseSound = new Audio("assets/audio/sfx_sounds_error3.m4a");
+// initialize first word
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+var secretWord = randomWord.split("");
+var workingWord = [];
 
-document.querySelector("#workingWordText").innerHTML = workingWord;
+secretWord.forEach(function(){
+    workingWord.push("_");
+});
+
+var workingWordDisp = workingWord.join(" ");
+document.querySelector("#workingWordDisp").innerHTML = workingWordDisp;
 
 document.onkeydown = function(event) {
 
+    document.querySelector("#startMsg").style.color = "#000";
+    music.play();
+    // grab key pressed and make lowercase
     var keyPressed = event.key.toLowerCase();
     var guessIndex = secretWord.indexOf(keyPressed);
+    var guessedAlready = badLettersArray.indexOf(keyPressed);
 
-    if (guessIndex < 0) {
-        guessesArray.push(keyPressed);
-        guessesLeft--;
+    // check if letter has already been guessed, then do nothing
+    if (guessedAlready >= 0) {
     }
 
-    else {        
+    // if guess is wrong, update guessesLeft and badLettersArray
+    else if (guessIndex < 0) {
+        badLettersArray.push(keyPressed);
+        guessesLeft--;
+        if (guessesLeft > 0){
+            noSound.play();
+        };
+    }
+
+    // if right, replace underscores with correct letters
+    else {
         for (i = 0; i < secretWord.length; i++) {
             if (secretWord[i] === keyPressed) {
                 workingWord[i] = keyPressed;
             };
-        }
+        };
+        if (workingWord.indexOf("_") >= 0){
+            yesSound.play();
+        };
     };
 
+    // check if word is complete or guesses spent, then reset game
 
-    document.querySelector("#workingWordText").innerHTML = workingWord;
-    document.querySelector("#winsText").innerHTML = "Wins: " + wins;
-    document.querySelector("#lossesText").innerHTML = "Losses: " + losses;
-    document.querySelector("#guessesLeftText").innerHTML = "Guesses left: " + guessesLeft;
-    document.querySelector("#guessesArrayText").innerHTML = guessesArray;
+    if (workingWord.indexOf("_") < 0) {
+        winSound.play();
+        wins++;
+        guessesLeft = 10;
+        badLettersArray = [];
+        // pick new random word
+        randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+        secretWord = randomWord.split("");
+        workingWord = [];
+        
+        secretWord.forEach(function(){
+            workingWord.push("_");
+        });        
+    }
+
+    else if (guessesLeft === 0) {
+        loseSound.play();
+        losses++;
+        guessesLeft = 10;
+        badLettersArray = [];
+        // pick new random word
+        randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+        secretWord = randomWord.split("");
+        workingWord = [];
+        
+        secretWord.forEach(function(){
+            workingWord.push("_");
+        });
+    };
+
+    // update display after each key press
+    workingWordDisp = workingWord.join(" ");
+    document.querySelector("#workingWordDisp").innerHTML = workingWordDisp;
+    document.querySelector("#winsDisp").innerHTML = "Wins: " + wins;
+    document.querySelector("#lossesDisp").innerHTML = "Losses: " + losses;
+    document.querySelector("#guessesLeftDisp").innerHTML = "Guesses left: " + guessesLeft;
+    document.querySelector("#badLettersArrayDisp").innerHTML = badLettersArray;
 
 };
